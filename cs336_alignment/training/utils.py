7a185a37ac738d6b757d623813a7327633fa41a4
PopSoda2002 <zhouhp.me@gmail.com>
2026-01-19 10:45:22 +0000

    Calc logp

--- cs336_alignment/training/utils.py
+++ cs336_alignment/training/utils.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import PreTrainedTokenizerBase
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
 
 def tokenize_prompt_and_output(prompt_strs: list[str], output_strs: list[str], tokenizer: PreTrainedTokenizerBase) -> dict[str, torch.Tensor]:
     """Tokenize the prompt and output strings, and construct a mask that is 1
@@ -29,3 +29,28 @@ def tokenize_prompt_and_output(prompt_strs: list[str], output_strs: list[str], t
         "labels": labels,
         "response_mask": response_mask,
     }
+
+@torch.inference_mode()
+def compute_entropy(logits: torch.Tensor) -> torch.Tensor:
+    """Compute the entropy of the logits."""
+    probs = torch.nn.functional.softmax(logits, dim=-1)
+    return -torch.sum(probs * torch.log(probs), dim=-1)
+
+@torch.inference_mode()
+def get_response_log_probs(model: PreTrainedModel, input_ids: torch.Tensor, labels: torch.Tensor, return_token_entropy: bool = False) -> dict[str, torch.Tensor]:
+    """Get the log-probs of the response given the prompt."""
+    model = model.to(input_ids.device)
+    model.eval()
+    logits = model(input_ids).logits
+    log_probs = torch.nn.functional.log_softmax(logits, dim=-1)
+    response_log_probs = log_probs.gather(
+        dim=-1,
+        index=labels.unsqueeze(-1)
+    ).squeeze(-1)
+    if return_token_entropy:
+        token_entropy = compute_entropy(logits)
+        return {
+            "log_probs": response_log_probs,
+            "token_entropy": token_entropy,
+        }
+    return {"log_probs": response_log_probs, "token_entropy": None}
--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 
-from cs336_alignment.training.utils import tokenize_prompt_and_output
+from cs336_alignment.training.utils import compute_entropy, get_response_log_probs, tokenize_prompt_and_output
 
 
 def run_tokenize_prompt_and_output(
@@ -84,7 +84,7 @@ def run_compute_group_normalized_rewards(
 
 def run_compute_entropy(logits: torch.Tensor) -> torch.Tensor:
     """Get the entropy of the logits (i.e., entropy of the final dimension)."""
-    raise NotImplementedError
+    return compute_entropy(logits)
 
 
 def run_get_response_log_probs(
@@ -116,7 +116,7 @@ def run_get_response_log_probs(
                 we have not masked out the token indices corresponding to the prompt
                 or padding; that is done in the train loop.
     """
-    raise NotImplementedError
+    return get_response_log_probs(model, input_ids, labels, return_token_entropy)
 
 
 def run_compute_naive_policy_gradient_loss(
